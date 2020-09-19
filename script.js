7dcf86bca0731bfc56deaf7a4e055c3eae32fb03
Njaal Wiik <njaal.wiik@gmail.com>
2020-09-19 13:19:16 +0200

    Implementet joke API and connected to test-to-speech api

--- script.js
+++ script.js
@@ -107,13 +107,37 @@ const VoiceRSS = {
 
 const apiKey = '';
 
-VoiceRSS.speech({
-  key: '9e7694c30e044947b823cce9276303f3',
-  src: 'Hello, world!',
-  hl: 'en-us',
-  v: 'Linda',
-  r: 0,
-  c: 'mp3',
-  f: '44khz_16bit_stereo',
-  ssml: false
-});
+// Get jokes from joke API
+const getJokes = async () => {
+  let joke = '';
+  const apiUrl =
+    'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=religious,political,racist,sexist';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    data.setup
+      ? (joke = `${data.setup} ...  ${data.delivery}`)
+      : (joke = data.joke);
+
+    tellMe(joke);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Pass joke to VoiceRSS API
+const tellMe = joke => {
+  VoiceRSS.speech({
+    key: '9e7694c30e044947b823cce9276303f3',
+    src: joke,
+    hl: 'en-us',
+    v: 'Linda',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false
+  });
+};
+
+getJokes();
